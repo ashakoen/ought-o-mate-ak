40347b444982837542267cc9ecabd9681dee6ac6
Asha Koen <ashakoen@gmail.com>
2024-09-28 17:17:51 -0400

    Add link to repo in footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import AdvancedTaskOptimizer from '../components/AdvancedTaskOptimizer'
+import { FaGithub } from 'react-icons/fa'
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
+    <main className="container mx-auto p-4 min-h-screen flex flex-col justify-between">
       <AdvancedTaskOptimizer />
+      <div className="w-full text-center p-4">
+        <a
+          href="https://github.com/ashakoen/ought-o-mate-ak"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-gray-700 flex items-center justify-center"
+        >
+          <FaGithub className="mr-2" />
+          View on GitHub
+        </a>
+      </div>
     </main>
   )
 }
